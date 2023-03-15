3350183a40003c12aff2ca45b7a3f3b20004f7ab
Andrii <andrii.stotskyi.u@gmail.com>
2023-03-15 14:59:47 +0200

    updated Task5

--- Laboratory_Work_1/src/Task5.java
+++ Laboratory_Work_1/src/Task5.java
@@ -1,28 +1,8 @@
-import java.util.Scanner;
-
 public class Task5 {
-        static int reverse(int n){
-       
-            int rev = 0; 
-            int rem;  
-             
-            while(n>0){
-                
-              rem = n%10;
-              rev = (rev*10) + rem;
-              n = n/10;
-            }
-            return rev;
-          }
-           
-         // Driver Function
-          public static void main (String[] args) {
-            Scanner sc = new Scanner(System.in);
-
-            System.out.print("Enter a three-digit number: ");
-            int n = sc.nextInt();
-               
-              System.out.print("Reversed Number is "+ reverse(n));
-          } 
-    
+  public static void main(String[] args) {
+    ReverseNumber a = new ReverseNumber();
+    a.Reverse();
+  }
 }
+
+  
