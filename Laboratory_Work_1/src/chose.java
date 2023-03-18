a1d72020f17a911aab047fc796ffd4e2c118add1
Andrii <andrii.stotskyi.u@gmail.com>
2023-03-18 10:54:58 +0200

    updated chose

--- Laboratory_Work_1/src/chose.java
+++ Laboratory_Work_1/src/chose.java
@@ -1,6 +1,33 @@
-public class Task1 {
-    public static void main(String[] args){
-        OperationOnNums obj = new OperationOnNums();
-        obj.PrintResults();
+public class chose {
+    public chose(String arg) {
+        run(arg);
     }
+
+    public static void run(String task) {
+        switch (task) {
+            case "Task1":
+                OperationOnNums obj = new OperationOnNums();
+                obj.PrintResults();
+                break;
+            case "Task2":
+                Sentence ob = new Sentence();
+                ob.CreateAndPrint();
+                break;
+            case "Task3":
+                User user = new User();
+                user.Init();
+                user.PrintInfo();
+                break;
+            case "Task4":
+                Car car = new Car();
+                car.Init();
+                car.PrintInfo();
+                break;
+            case "Task5":
+                ReverseNumber a = new ReverseNumber();
+                a.Reverse();
+                break;
+        }
+    }
+
 }
